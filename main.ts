351b7b33379906c4bbf3fd9e5277eefc1419ecaa
StrnadMarek <149717630+StrnadMarek@users.noreply.github.com>
2024-05-21 18:28:30 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,36 @@
+basic.showIcon(IconNames.Angry)
+radio.setGroup(210)
 
+let backward: number = 0
+let right: number = 0
+let rightMotor: number = 0
+let leftMotor: number = 0
+let block: boolean = false
+
+radio.onReceivedValue(function (name, value) {
+    if (name.compare("mgy") == 0) {
+        right = value
+    }
+    if (name.compare("mgx") == 0) {
+        backward = value
+    }
+    if (name.compare("block") == 0) {
+        block = true
+    }
+    if (name.compare("unblock") == 0) {
+        block = false
+    }
+    if (block) {
+        rightMotor = 0
+        leftMotor = 0
+    } else {
+        // Kombinace pro pohyb a zatáčení
+        rightMotor = backward - right
+        leftMotor = backward + right
+    }
+})
+
+basic.forever(function () {
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, rightMotor)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, leftMotor)
+})
