7d5475483e9c592141373ce15655c2b2bdb207d9
StrnadMarek <149717630+StrnadMarek@users.noreply.github.com>
2024-05-21 18:32:51 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ radio.setGroup(210)
 let strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
 let strip2 = neopixel.create(DigitalPin.P16, 24, NeoPixelMode.RGB)
 strip.showRainbow(1, 360)
+strip2.showRainbow(1, 360)
 
 let backward: number = 0
 let right: number = 0
